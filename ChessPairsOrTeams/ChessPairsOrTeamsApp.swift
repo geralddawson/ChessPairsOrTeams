345f29cd861e6900a2c5f25e6ef5ebb95c02b220
geralddawson <jerripot@gmail.com>
2026-04-13 04:59:11 +1000

    Initial Commit

---
+++ ChessPairsOrTeams/ChessPairsOrTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessPairsOrTeamsApp.swift
+//  ChessPairsOrTeams
+//
+//  Created by Gerald Dawson on 13/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChessPairsOrTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
